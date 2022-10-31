b97cb6812125e5fb3ed6f7c5b3781513c11a97a6
Yevheniia-Mikhalova <114936974+Yevheniia-Mikhalova@users.noreply.github.com>
2022-11-01 01:01:35 +0200

    function

--- Documents/JavaSkript/script.js
+++ Documents/JavaSkript/script.js
@@ -1,30 +1,31 @@
-const client = {
-   name: "Sam", age: 16, weight: 40, height: 150
-   //name: "Lola", age: 28, weight: 63, height: 182
-   //name: "Tom", age: 35, weight: 90, height: 175
-   //name: "Kate", age: 62, weight: 50, height: 160
-};
+function doIt() {
+  let client = {
+    name: "Sam",
+    age: 16,
+    weight: 100,
+    height: 190,
+  };
 
-if (client.age > 18 && client.age < 60 ) {
-  console.log("Вітаю, ви зареєстровані")
-} else {
-  console.log("Вибачте, ви не зареєстровані")
-}
+  if (client.age > 18 && client.age < 60 ) {
+  } else if (client.age < 18 ){
+    alert("Вибач, але ти ще малий")
+  }
+  
+  if (client.weight > 60 && client.weight < 120 ) {
+  } else if(client.weight > 120 ){
+    alert("Упс, треба схуднути")
+  }
+  
+  if (client.height > 160 && client.height < 195 ) {
+  } else if(client.height > 195 ) {
+    alert("Вибачте, вы не зареєстровані")
+  } else {
+    alert("Вітаю, ви зареєстровані")
+  }
 
-if (client.weight > 60 && client.weight < 120 ) {
-  console.log("Вітаю, ви зареєстровані")
-} else {
-  console.log("Вибачте, ви не зареєстровані")
-}
 
-if (client.height > 160 && client.height < 195 ) {
-  console.log("Вітаю, ви зареєстровані")
-} else {
-  console.log("Вибачте, ви не зареєстровані")
-}
-
-
-function registration() {
+function registration(){
   const fullNameEl = document.getElementById("fullName");
   const fullName = fullNameEl.value;
+}
 }
