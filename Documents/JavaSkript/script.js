dbf65eeeaa269d3fb61702b191b6e1cd3d861844
Yevheniia-Mikhalova <114936974+Yevheniia-Mikhalova@users.noreply.github.com>
2022-11-01 14:05:31 +0200

    error correction

--- Documents/JavaSkript/script.js
+++ Documents/JavaSkript/script.js
@@ -1,31 +1,26 @@
-function doIt() {
-  let client = {
-    name: "Sam",
-    age: 16,
-    weight: 100,
-    height: 190,
-  };
+function validateData() {
+  const nameEl = document.getElementById("name");
+  const ageEl = document.getElementById("age");
+  const weightEl = document.getElementById("weight");
+  const heightEl = document.getElementById("height");
 
-  if (client.age > 18 && client.age < 60 ) {
-  } else if (client.age < 18 ){
-    alert("Вибач, але ти ще малий")
-  }
-  
-  if (client.weight > 60 && client.weight < 120 ) {
-  } else if(client.weight > 120 ){
-    alert("Упс, треба схуднути")
-  }
-  
-  if (client.height > 160 && client.height < 195 ) {
-  } else if(client.height > 195 ) {
-    alert("Вибачте, вы не зареєстровані")
+  const name = nameEl.value;
+  const age = ageEl.value;
+  const weight = weightEl.value;
+  const height = heightEl.value;
+
+  if (age < 18 || age > 60 ) {
+     alert("Sorry, your age is wrong");
+  } else if (weight < 60 || weight > 120 ) {
+     alert("Sorry, your weight is wrong");
+  } else if (height < 160 || height > 195 ) {
+    alert("Sorry, your height is wrong");
   } else {
-    alert("Вітаю, ви зареєстровані")
+    alert("Congratulations, you are registered");
+
+    nameEl.value = "";
+    ageEl.value = "";
+    weightEl.value = "";
+    heightEl.value = "";
   }
-
-
-function registration(){
-  const fullNameEl = document.getElementById("fullName");
-  const fullName = fullNameEl.value;
-}
 }
